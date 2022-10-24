c486b66949d1c6a8fbe5535c045bfe95940d9cbe
Mudasir Issah <danladi496@gmail.com>
2022-10-24 09:50:28 +0000

    added book array and add function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('working');
+// creating array of objects
+const books = [
+    {
+        title: 'How to Code in Javascript',
+        author: 'Mudasir Issah'
+    },
+    {
+        title: 'How to Code in Ruby',
+        author: 'Hein Tay Zar'
+    }
+];
+
+let button = document.getElementById('add');
+
+button.addEventListener('click', (e) => {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    books.push({title,author});
+    localStorage.setItem('book',JSON.stringify(books));
+    e.preventDefault();
+})
